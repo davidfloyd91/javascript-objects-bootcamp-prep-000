2f60675d03b7d30e80a77b45f043536e493383a1
David Floyd <davidfloyd91@gmail.com>
2018-10-27 21:15:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,8 @@ var playlist = {
   Nirvana: 'Lithium'
 };
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {artistName: songTitle});
+  return playlist.artistName = songTitle;
+}
+function removeFromPlaylist(playslist, artistName) {
+  
 }
